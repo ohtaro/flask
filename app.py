23ab7abc064dc4d538a383eae3c22097478b442b
ohta yusuke <ohta.yusuke@nttcom.co.jp>
2019-04-05 17:56:47 +0900

    fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'Hello kubernetes world'
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=80)
