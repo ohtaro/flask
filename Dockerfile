ad1524d4c518e6a2b730f7648c4250eabab48767
ohta yusuke <ohta.yusuke@nttcom.co.jp>
2019-04-08 11:09:18 +0900

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM python:3.7
 
-RUN pip install flask \
+RUN pip install ddtrace flask \
     && mkdir /app
 
 COPY app.py /app/
 WORKDIR /app
 
-CMD ["python", "app.py"]
+CMD ["ddtrace-run", "python", "app.py"]
--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,4 @@
 set -e
 envsubst < ./k8s/kustomization-template.yaml > "./k8s/base/kustomization.yaml"
  
-gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/${IMAGE_NAME}:$CIRCLE_SHA1
- 
 kubectl kustomize "./k8s/base" | kubectl apply -f -
